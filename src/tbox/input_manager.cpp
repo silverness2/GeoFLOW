213d1d500a0b82d59c7e2c7baa1559d58d4ba1ee
BryanFlynt-NOAA <bryan.flynt@noaa.gov>
2019-02-20 11:52:03 -0700

    Added -h to ignore input files (#12)

--- src/tbox/input_manager.cpp
+++ src/tbox/input_manager.cpp
@@ -8,16 +8,29 @@
 #include "tbox/command_line.hpp"
 #include "tbox/input_manager.hpp"
 #include "tbox/mpixx.hpp"
+#include "tbox/pio.hpp"
 
 namespace geoflow {
 namespace tbox {
 
 PropertyTree InputManager::ptree_;
 
+//
+// TODO: Remove the -h requirement
+// It breaks encapsulation for InputManager to check for other
+// classes command line flags.  An easy fix would be to require
+// all runs to use the -i flag to specify the file.  If no -i is
+// used then no file is read.
+//
 void
 InputManager::initialize(int argc, char* argv[]){
-	std::string file_name = "input.jsn";
 	CommandLine cline(argc,argv);
+	if( cline.tagExists("-h") ){
+		pio::pout << "IGNORE INPUT FILE: '-h' found on command line\n";
+		return; // "-h" tag negates any file reading
+	}
+
+	std::string file_name = "input.jsn";
 	if( cline.tagExists("-i") ){
 		file_name = cline.getValue("-i");
 	}
