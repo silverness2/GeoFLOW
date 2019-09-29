74ff8de42d46474d16c9b12bb9390713942f88bd
Duane Rosenberg <duane.rosenberg@enki.fsl.noaa.gov>
2019-09-29 18:35:47 +0000

    Read and use E0 rather than u0 or f0 for normalization, noting new GMTK::normalize function

--- src/cdg/user/state/ginitv.cpp
+++ src/cdg/user/state/ginitv.cpp
@@ -36,7 +36,7 @@ GBOOL impl_abc_box(const PropertyTree &ptree, GString &sconfig, GGrid &grid, Tim
 
   GINT         kdn, kup, p, pdef;
   GSIZET       nn ;
-  GFTYPE       A, B, C, pi2, u0, x, y, z;
+  GFTYPE       A, B, C, E0, pi2, x, y, z;
   PropertyTree vtree ;
   GTVector<GTVector<GFTYPE>>
               *xnodes = &grid.xNodes();
@@ -56,7 +56,7 @@ GBOOL impl_abc_box(const PropertyTree &ptree, GString &sconfig, GGrid &grid, Tim
 #if defined(_G_IS3D)
   C     = vtree.getValue<GFTYPE>("C", 1.1);
 #endif
-  u0    = vtree.getValue<GFTYPE>("u0", 1.0);
+  E0    = vtree.getValue<GFTYPE>("E0", 1.0);
   nn    = (*xnodes)[0].size();
 
 #if defined(_G_IS2D)
@@ -92,7 +92,7 @@ GBOOL impl_abc_box(const PropertyTree &ptree, GString &sconfig, GGrid &grid, Tim
 
 #endif
 
-  GMTK::normalizeL2(grid, u, utmp, u0);
+  GMTK::normalizeL2(grid, u, utmp, E0);
 
   return TRUE;
 
@@ -121,7 +121,7 @@ GBOOL impl_abc_icos(const PropertyTree &ptree, GString &sconfig, GGrid &grid, Ti
 
   GINT         kdn, kup, p, pdef;
   GSIZET       nn ;
-  GFTYPE       A, B, C, u0, x, y, z;
+  GFTYPE       A, B, C, E0, x, y, z;
   GFTYPE       alat, along, r;
   PropertyTree vtree ;
   GTVector<GTVector<GFTYPE>*> 
@@ -144,7 +144,7 @@ GBOOL impl_abc_icos(const PropertyTree &ptree, GString &sconfig, GGrid &grid, Ti
 #if defined(_G_IS3D)
   C     = vtree.getValue<GFTYPE>("C", 1.1);
 #endif
-  u0    = vtree.getValue<GFTYPE>("u0", 1.0);
+  E0    = vtree.getValue<GFTYPE>("E0", 1.0);
   nn    = (*xnodes)[0].size();
 
   usph[0] = utmp[0]; // ulat storage
@@ -185,7 +185,7 @@ GBOOL impl_abc_icos(const PropertyTree &ptree, GString &sconfig, GGrid &grid, Ti
 
 #endif
  
-  GMTK::normalizeL2(grid, u, utmp, u0);
+  GMTK::normalizeL2(grid, u, utmp, E0);
 
 
   return TRUE;
