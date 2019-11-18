ea657885404023955e4e400adb3d49d224ce1d31
Duane Rosenberg <Duane.Rosenberg@noaa.gov>
2019-11-18 00:38:36 +0000

    change fudge-factor on dX in init_ggfx to 0.1 for icos grids

--- src/apps/geoflow_cdg.cpp
+++ src/apps/geoflow_cdg.cpp
@@ -691,7 +691,7 @@ void init_ggfx(PropertyTree &ptree, GGrid &grid, GGFX<GFTYPE> *&ggfx)
     rad   = gtree.getValue<GFTYPE>("radius");
     delta[0] = 0.5*grid.minlength()/(rad*pmax*pmax);
     delta[1] = grid.minlength()/(rad*pmax*pmax);
-    for ( auto j=0; j<dX.size(); j++ ) dX[j] = 0.05 *delta[j];
+    for ( auto j=0; j<dX.size(); j++ ) dX[j] = 0.1 *delta[j];
     gmorton.setType(GMORTON_STACKED);
 //  gmorton.setType(GMORTON_INTERLEAVE);
 #else
