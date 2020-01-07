0db00fba3421dbd224177c0393a96a2db8ae772f
Duane Rosenberg <Duane.Rosenberg@noaa.gov>
2020-01-07 21:04:48 +0000

    .

--- src/cdg/user/force/ginitfv.cpp
+++ src/cdg/user/force/ginitfv.cpp
@@ -121,7 +121,7 @@ GBOOL impl_abc_icos(const PropertyTree &ptree, GString &sconfig, GGrid &grid, Ti
   GINT         kdn, kn, kup, p, pdef;
   GSIZET       nn ;
   GFTYPE       A, B, C, E0, x, y, z;
-  GFTYPE       alat, along, r, kn;
+  GFTYPE       alat, along, r;
   PropertyTree vtree ;
   GTVector<GTVector<GFTYPE>*>
                usph(GDIM);
