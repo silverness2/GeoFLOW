9bb96a1b9a95cc771e9e44a4d294f228ff369d3e
Duane Rosenberg <Duane.Rosenberg@noaa.gov>
2020-01-07 21:00:40 +0000

    correct compile error

--- src/cdg/user/force/ginitfv.cpp
+++ src/cdg/user/force/ginitfv.cpp
@@ -121,7 +121,7 @@ GBOOL impl_abc_icos(const PropertyTree &ptree, GString &sconfig, GGrid &grid, Ti
   GINT         kdn, kup, p, pdef;
   GSIZET       nn ;
   GFTYPE       A, B, C, E0, x, y, z;
-  GFTYPE       alat, along, r;
+  GFTYPE       alat, along, r, kn;
   PropertyTree vtree ;
   GTVector<GTVector<GFTYPE>*>
                usph(GDIM);
