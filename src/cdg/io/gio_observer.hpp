d124bb51a19e1e674fbeb1b78a24862589601c2b
Duane Rosenberg <duane.rosenberg@enki.fsl.noaa.gov>
2020-02-05 19:35:56 +0000

    Rename from gposixio_observer.* to gio_observer.*

---
+++ src/cdg/io/gio_observer.hpp
@@ -0,0 +1,87 @@
+//==================================================================================
+// Module       : gio_observer.ipp
+// Date         : 1/28/20 (DLR)
+// Description  : Observer object for carrying out binary output
+// Copyright    : Copyright 2020. Colorado State University. All rights reserved.
+// Derived From : ObserverBase.
+//==================================================================================
+#if !defined(_GIO_OBSERVER_HPP)
+#define _GIO_OBSERVER_HPP
+
+#include "gtvector.hpp"
+#include "ggrid.hpp"
+#include "pdeint/equation_base.hpp"
+#include "pdeint/observer_base.hpp"
+#include "tbox/property_tree.hpp"
+#include "tbox/mpixx.hpp"
+#include "gio.h"
+
+using namespace geoflow::pdeint;
+using namespace std;
+
+
+template<typename OBSType>
+class GIOObserver : public ObserverBase<OBSType>
+{
+
+public:
+        using Equation    = OBSType;
+        using EqnBase     = EquationBase<OBSType>;
+        using EqnBasePtr  = std::shared_ptr<EqnBase>;
+        using IOBaseType  = IOBase<EquationType>;
+        using IOBasePtr   = std::shared_ptr<IOBaseType>;
+        using State       = typename Equation::State;
+        using StateInfo   = typename Equation::StateInfo;
+        using Grid        = typename Equation::Grid;
+        using Value       = typename Equation::Value;
+        using Derivative  = typename Equation::Derivative;
+        using Time        = typename Equation::Time;
+        using Jacobian    = typename Equation::Jacobian;
+        using Size        = typename Equation::Size;
+
+//      using ObserverBase<OBSType>::ObsType;
+//      using OBS_CYCLE = typename ObserverBase<OBSType>::ObsType::OBS_CYCLE;
+//      using OBS_TIME  = typename ObserverBase<OBSType>::OBS_TIME;
+
+        static_assert(std::is_same<State,GTVector<GTVector<GFTYPE>*>>::value,
+               "State is of incorrect type");
+        static_assert(std::is_same<Derivative,GTVector<GTVector<GFTYPE>*>>::value,
+               "Derivative is of incorrect type");
+        static_assert(std::is_same<Grid,GGrid>::value,
+               "Grid is of incorrect type");
+
+                           GIOObserver() = delete;
+                           GIOObserver(const EqnBasePtr &equation, const IOBasePtr &io_base_ptr,
+                                       Grid &grid, typename ObserverBase<EquationType>::Traits &traits);
+
+                          ~GIOObserver() = default;
+                           GIOObserver(const GIOObserver &a) = default;
+                           GIOObserver &operator=(const GIOObserver &bu) = default;
+
+        void               observe_impl(const Time &t, const State &u, const State &uf);
+
+private:
+// Private methods:
+        void               init(const Time &t, const State &u);
+        void               print_derived(const Time &t, const State &u, GIOTraits &traits, const GC_COMM &comm);
+// Private data:
+        GBOOL              bprgrid_    ;// print grid flag
+        GBOOL              bInit_      ;// is initialized?
+        GSIZET             cycle_last_ ;// most recent output cycle
+        GSIZET             cycle_      ;// continuously-running cycle
+        GSIZET             ocycle_     ;// output cycle number
+        GFTYPE             time_last_  ;// most recent output time
+        GTVector<GINT>     state_index_;// list of state indices to print
+        GTVector<GString>  state_names_;// list of names of state files
+        GTVector<GString>  grid_names_ ;// list of names of grid comp files
+        IOBasePtr         *pio_obj_    ;// ptr to IO object
+        GString            sidir_      ;// directory from which to read (e.g., for restart)
+        GString            sodir_      ;// directory in which to write
+    
+
+};
+
+#include "gposixio_observer.ipp"
+
+#endif
+
