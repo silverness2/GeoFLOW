a9e8f0ae3e615b81fc9d1373f5f6a88035fdd409
Duane Rosenberg <duane.rosenberg@enki.fsl.noaa.gov>
2020-03-13 18:02:59 +0000

    Rename to gterrain_specbox_user.*; add gterrain_specsph_user.* as place holder for terrain specification namespaces for spherical grids.

---
+++ src/cdg/user/terrain/gterrain_specsph_user.cpp
@@ -0,0 +1,163 @@
+// Module       : gterrain_specsph.hpp
+// Date         : 7/11/19 (DLR)
+// Description  : Terrain specification function implementations provided
+//                by user for spherical grids (2d and 3d).
+// Copyright    : Copyright 2020. Colorado State University. All rights reserved.
+// Derived From : none.
+//==================================================================================
+#include "gterrain_specsph_user.hpp"
+
+
+namespace gterrain_specsph {
+
+
+
+
+//**********************************************************************************
+//**********************************************************************************
+// METHOD : impl_sphgauss_sphrange
+// DESC   : Specify terrange as a 'Gaussian mountain range' with
+//          info by ttree. Intended for sph grids
+// ARGS   : ptree: main prop tree 
+//          grid : grid
+//          utmp : tmp arrays
+//          xb   : terrain vectors
+// RETURNS: TRUE on success; else FALSE 
+//**********************************************************************************
+GBOOL impl_sphgauss_range(const PropertyTree &ptree, GGrid &grid, State &utmp, State &xb)
+{
+
+  assert(FALSE && "Under construction");
+
+  GString  sterrain  = ptree.getPropertyTree("terrain_type");
+
+  assert("sphgauss_range" == sterrain && "Invalid terrain_type");
+
+  PropertyTree ttree     = ttree.getPropertyTree(sterrain);
+  PropertyTree sphptree  = ptree.getPropertyTree("grid_sph");
+
+  GSIZET nxy = (*xnodes)[0].size();
+  GFTYPE dx, dy, eps;
+  GTVector<GTVector<GFTYPE>> *xnodes = &grid.xNodes();
+  GTPoint<GFTYPE>  P0(3);
+  
+  std::vector<GFTYPE> x0   = ttree.getArray<GFTYPE>("x0");
+  if ( GDIM == 3 ) 
+  std::vector<GFTYPE> y0   = ttree.getArray<GFTYPE>("y0");
+  std::vector<GFTYPE> xsig = ttree.getArray<GFTYPE>("xsigma");
+  if ( GDIM == 3 ) 
+  std::vector<GFTYPE> ysig = ttree.getArray<GFTYPE>("ysigma");
+  std::vector<GFTYPE> h0   = ttree.getArray<GFTYPE>("h0");
+
+  std::vector<GFTYPE> xyz0 = sphptree.getArray<GFTYPE>("xyz0");
+//std::vector<GFTYPE> dxyz = sphptree.getArray<GFTYPE>("delxyz");
+  P0 = xyz0; 
+
+  eps =  100*std::numeric_limits<GFTYPE>::epsilon();
+
+  // Build terrain height vector:
+#if defined(_IS2D)
+  for ( auto m=0; m<x0.size(); m++ ) {   // for each Gaussian lump
+    xb[1] = 0.0;
+    for ( auto j=0; j<nxy; j++ ) {
+      dx        = (*xnodes)[0][j] - x0[m];
+      if ( xb[1][j] == 
+      if ( FUZZYEQ(P0.x2,(*xnodes)[1][j],eps)
+        xb[1][j] += h0[m]*exp(-pow(dx,2) / (sig[m]*sig[m]) )
+    }
+  }
+#elif defined(_IS3D)
+  xb[2] = 0.0;
+  for ( auto m=0; m<x0.size(); m++ ) {   // for each Gaussian lump
+    for ( auto j=0; j<nxy; j++ ) {
+      dx        = (*xnodes)[0][j] - x0[m];
+      dy        = (*xnodes)[1][j] - y0[m];
+      if ( FUZZYEQ(P0.x3,(*xnodes)[2][j],eps)
+        xb[2][j] += h0[m]*exp(-dx*dx / (xsig[m]*xsig[m])) * exp(-dy*dy / (ysig[m]*ysig[m]));
+    }
+  }
+#endif
+
+  return TRUE;
+
+} // end, impl_sphgauss_range
+
+
+//**********************************************************************************
+//**********************************************************************************
+// METHOD : impl_sphpoly_range
+// DESC   : Specify terrange as a inverse polynomials of the form:
+//               h = h0/[(x-x0)^2/a^2 + (y-y0)^2/b^2]^(p/2)
+//           Intended for sph grids.
+//          
+// ARGS   : ptree: main prop tree 
+//          grid : grid
+//          utmp : tmp arrays
+//          xb   : terrain vectors
+// RETURNS: TRUE on success; else FALSE 
+//**********************************************************************************
+GBOOL impl_sphrange_range(const PropertyTree &ptree, GGrid &grid, State &utmp, State &xb)
+{
+
+  assert(FALSE && "Under construction");
+
+
+  GString  sterrain  = ptree.getPropertyTree("terrain_type");
+
+  assert("sphpoly_range" == sterrain && "Invalid terrain_type");
+
+  PropertyTree ttree     = ttree.getPropertyTree(sterrain);
+//PropertyTree sphptree  = ptree.getPropertyTree("grid_sph");
+
+  GSIZET nxy = (*xnodes)[0].size();
+  GFTYPE dx, dy;
+  GTVector<GTVector<GFTYPE>> *xnodes = &grid.xNodes();
+  GTPoint<GFTYPE>  P0(3);
+  
+  std::vector<GFTYPE> pexp = ttree.getArray<GFTYPE>("exponent", 3);
+  std::vector<GFTYPE> x0   = ttree.getArray<GFTYPE>("x0");
+  if ( GDIM == 3 ) 
+  std::vector<GFTYPE> y0   = ttree.getArray<GFTYPE>("y0");
+  std::vector<GFTYPE> xsig = ttree.getArray<GFTYPE>("xsigma");
+  if ( GDIM == 3 ) 
+  std::vector<GFTYPE> ysig = ttree.getArray<GFTYPE>("ysigma");
+  std::vector<GFTYPE> h0   = ttree.getArray<GFTYPE>("h0");
+
+  std::vector<GFTYPE> xyz0 = sphptree.getArray<GFTYPE>("xyz0");
+//std::vector<GFTYPE> dxyz = sphptree.getArray<GFTYPE>("delxyz");
+  P0 = xyz0; 
+
+  eps =  100*std::numeric_limits<GFTYPE>::epsilon();
+
+
+  // Build terrain height vector:
+#if defined(_IS2D)
+  for ( auto m=0; m<x0.size(); m++ ) {   // for each Gaussian lump
+    xb[1] = 0.0;
+    for ( auto j=0; j<nxy; j++ ) {
+      dx        = (*xnodes)[0][j] - x0[m];
+      if ( xb[1][j] == 
+      if ( FUZZYEQ(P0.x2,(*xnodes)[1][j],eps)
+        xb[1][j] += h0[m]/( pow( dx*dx/(xsigma*xsigma) + 1, pexp[m]/2) );
+    }
+  }
+#elif defined(_IS3D)
+  xb[2] = 0.0;
+  for ( auto m=0; m<x0.size(); m++ ) {   // for each Gaussian lump
+    for ( auto j=0; j<nxy; j++ ) {
+      dx        = (*xnodes)[0][j] - x0[m];
+      dy        = (*xnodes)[1][j] - y0[m];
+      if ( FUZZYEQ(P0.x3,(*xnodes)[2][j],eps)
+        xb[2][j] += h0[m]/( pow( dx*dx/(xsigma*xsigma) + dy*dy/(ysigma*ysigma) + 1, pexp[m]/2) );
+    }
+  }
+#endif
+
+  return TRUE;
+
+} // end, impl_sphpoly_range
+
+
+
+
+} // end, ginitbdy namespace
---
+++ src/cdg/user/terrain/gterrain_specsph_user.hpp
@@ -0,0 +1,32 @@
+//==================================================================================
+// Module       : gterrain_specsph.hpp
+// Date         : 7/11/19 (DLR)
+// Description  : Terrain specification function implementations provided
+//                by user for spherical grids (2d and 3d).
+// Copyright    : Copyright 2020. Colorado State University. All rights reserved.
+// Derived From : none.
+//==================================================================================
+#if !defined(_GTERRAINSPEC_USER_HPP)
+#define _GTERRAINSPEC_USER_HPP
+
+#include "tbox/property_tree.hpp"
+#include "gtypes.h"
+#include "gtvector.hpp"
+#include "ggrid.hpp"
+
+
+using namespace geoflow;
+using namespace geoflow::tbox;
+
+typedef GFTYPE                      Time;
+typedef GTVector<GTVector<GFTYPE>*> State;
+
+
+namespace gterrain_specbox
+{
+
+GBOOL impl_gauss_range     (const PropertyTree& stree, GGrid &grid,  State &utmp, State &xb);
+GBOOL impl_poly_range      (const PropertyTree& stree, GGrid &grid,  State &utmp, State &xb);
+};
+
+#endif
