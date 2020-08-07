0bd330e7f4a5bc93688594a0c86aba4bccf866c5
Duane Rosenberg <Duane.Rosenberg@noaa.gov>
2020-08-07 16:57:50 -0600

    Rename CPL, CPI to CVL, CVI, for liquid and ice water

--- src/cdg/include/gphysics.h
+++ src/cdg/include/gphysics.h
@@ -10,13 +10,12 @@
 
 #include "configure.hpp"
 
-  #define CP      1004.0    // sp. heat const press, dry air J/kg-K
+  #define CPD     1004.0    // sp. heat const press, dry air J/kg-K
   #define CPV     1885.0    // sp. heat const press, liq. water J/Kg-J
-  #define CPL     4186.0    // sp. heat const press, liq. water J/Kg-J
-  #define CPI     2090.0    // sp. heat const press, ice J/Kg-J
-  #define CV       717.0    // sp. heat const vol, dry air J/Kg-J
+  #define CVD      717.0    // sp. heat const vol, dry air J/Kg-J
   #define CVV     1424.0    // sp. heat const vol, water vapor J/Kg-J
   #define CVI     2108.0    // sp. heat const vol, ice J/Kg-J
+  #define CVL     4186.0    // sp. heat const press, liq. water J/Kg-J
   #define GG         9.81   // accel due to gravity m/s^2
   #define LV0        2.50e6 // latent heat of vaporization, reference J/kg
   #define RD       287.0    // gas const for dry air J/kg-K
