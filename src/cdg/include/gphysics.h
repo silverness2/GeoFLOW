36b4a1cfd8e8993c1c03530e733f22fa08fcf574
Duane Rosenberg <Duane.Rosenberg@noaa.gov>
2020-08-07 20:09:26 +0000

    Correct comments

--- src/cdg/include/gphysics.h
+++ src/cdg/include/gphysics.h
@@ -10,18 +10,18 @@
 
 #include "configure.hpp"
 
-  #define CP      1004.0    # sp. heat const press, dry air J/kg-K
-  #define CPV     1885.0    # sp. heat const press, liq. water J/Kg-J
-  #define CPL     4186.0    # sp. heat const press, liq. water J/Kg-J
-  #define CPI     2090.0    # sp. heat const press, ice J/Kg-J
-  #define CV       717.0    # sp. heat const vol, dry air J/Kg-J
-  #define CVV     1424.0    # sp. heat const vol, water vapor J/Kg-J
-  #define CVI     2108.0    # sp. heat const vol, ice J/Kg-J
-  #define GG         9.81   # accel due to gravity m/s^2
-  #define LV0        2.50e6 # latent heat of vaporization, reference J/kg
-  #define RD       287.0    # gas const for dry air J/kg-K
-  #define RV       461.0    # gas const for water vapor J/kg-K
-  #define T0       273.15   # ref temp K
+  #define CP      1004.0    // sp. heat const press, dry air J/kg-K
+  #define CPV     1885.0    // sp. heat const press, liq. water J/Kg-J
+  #define CPL     4186.0    // sp. heat const press, liq. water J/Kg-J
+  #define CPI     2090.0    // sp. heat const press, ice J/Kg-J
+  #define CV       717.0    // sp. heat const vol, dry air J/Kg-J
+  #define CVV     1424.0    // sp. heat const vol, water vapor J/Kg-J
+  #define CVI     2108.0    // sp. heat const vol, ice J/Kg-J
+  #define GG         9.81   // accel due to gravity m/s^2
+  #define LV0        2.50e6 // latent heat of vaporization, reference J/kg
+  #define RD       287.0    // gas const for dry air J/kg-K
+  #define RV       461.0    // gas const for water vapor J/kg-K
+  #define T0       273.15   // ref temp K
 
 #endif // !defined(_GPHYSICS_H)
 
