1bd366274c893141443acccacf49a46de320a8e4
Duane Rosenberg <Duane.Rosenberg@noaa.gov>
2020-10-21 14:42:48 -0600

    set #define DO_FACE

--- src/cdg/sem/gstressen.hpp
+++ src/cdg/sem/gstressen.hpp
@@ -31,8 +31,8 @@
 #include "gmtk.hpp"
 #include "pdeint/equation_base.hpp"
 
-#define  USE_STOKES
-#undef   DO_FACE
+#define   USE_STOKES
+#define   DO_FACE
 
 template<typename TypePack>
 class GStressEnOp
