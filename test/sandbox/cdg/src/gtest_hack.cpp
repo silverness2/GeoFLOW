f2d1a17abcb9d87e21428875a1f46330b990a5c9
Duane Rosenberg <Duane.Rosenberg@noaa.gov>
2020-10-23 15:48:44 -0600

    Misc mods to build

--- test/sandbox/cdg/src/gtest_hack.cpp
+++ test/sandbox/cdg/src/gtest_hack.cpp
@@ -7,47 +7,12 @@
 // Derived From : 
 //==================================================================================
 
-#include "gexec.h"
-#include "gtypes.h"
-#include <cstdio>
-#include <unistd.h>
-#include <iostream>
-#include "gptl.h"
-#include <memory>
-#include <cstdlib>
-#include <cassert>
-#include <random>
-#include "gcomm.hpp"
-#include "ggfx.hpp"
-#include "gllbasis.hpp"
-#include "gmass.hpp"
-#include "gmorton_keygen.hpp"
-#include "ggrid_factory.hpp"
-#include "gmtk.hpp"
-#include "tbox/property_tree.hpp"
-#include "tbox/mpixx.hpp"
-#include "tbox/global_manager.hpp"
-#include "tbox/input_manager.hpp"
-#include "gtools.h"
-#include "gio.h"
-
-using namespace geoflow::tbox;
-using namespace std;
-
-#define   _DO_REFDERIV
-#undef   _DO_REFDERIVW
-#if defined(_DO_REFDERIVW) && defined(_DO_REFDERIV)
-  #error "Cannot define both _DO_REFDERIVW AND _DO_REFDERIV"
-#endif
-
-GGrid *grid_ = NULLPTR;
-
-typedef  GTVector<GTVector<GFTYPE>*> State;
+#include "gtest_hack.h"
 
 int main(int argc, char **argv)
 {
 
-    GINT    errcode ;
+    GINT    errcode=0 ;
     GINT    nc=GDIM; // no. coords
     GFTYPE  eps=10.0*std::numeric_limits<GFTYPE>::epsilon();
     GFTYPE  dnorm, told=0.0, tnew=0.0;
@@ -81,9 +46,9 @@ int main(int argc, char **argv)
     polyptree   = ptree.getPropertyTree("poly_test");
     nc          = GDIM;
     assert(sgrid == "grid_box"); // use only Cartesian grids
-    assert(pstd  >= GDIM      ); 
+    assert(pstd.size() >= GDIM); 
 
-    pvec.resize(pstd.size(()); pvec = pstd; pvec.range(0,GDIM-1);
+    pvec.resize(pstd.size()); pvec = pstd; pvec.range(0,GDIM-1);
     
     
     // Set GTPL options:
@@ -102,30 +67,31 @@ int main(int argc, char **argv)
     }
     
     // Create grid:
-    grid_ = GGridFactory::build(ptree, gbasis, comm);
+    ObsTraitsType binobstraits;
+    grid_ = GGridFactory<MyTypes>::build(ptree, gbasis, pIO_, binobstraits, comm);
 
+#if 0 // May define if continuity is requiured
+    // Initialize gather/scatter operator:
+    GGFX<GFTYPE> ggfx;
+    init_ggfx(ptree, *grid_, ggfx);
+#endif
 
     /////////////////////////////////////////////////////////////////
     ////////////////////// Allocate arrays //////////////////////////
     /////////////////////////////////////////////////////////////////
-    GPTLstart("do_gather_op");
-    // Initialize gather/scatter operator:
-    GGFX<GFTYPE> ggfx;
-    init_ggfx(ptree, *grid_, ggfx);
-    GPTLstop("do_gather_op");
 
     // Create state and tmp space:
-    GTVector<GTVector<GFTYPE>*> utmp (3);
-    GTVector<GTVector<GFTYPE>*> dunew(nc);
-    GTVector<GTVector<GFTYPE>*> duold(nc);
-    GTVector<GTVector<GFTYPE>*> da   (nc);
-    GTVector<GTVector<GFTYPE>*> du   (nc);
-    GTVector<GFTYPE>            diff, u;
+    State     utmp (3);
+    State     dunew(nc);
+    State     duold(nc);
+    State     da   (nc);
+    State     du   (nc);
+    StateComp diff, u;
     
-    for ( auto j=0; j<utmp .size(); j++ ) utmp [j] = new GTVector<GFTYPE>(grid_->size());
-    for ( auto j=0; j<duold.size(); j++ ) duold[j] = new GTVector<GFTYPE>(grid_->size());
-    for ( auto j=0; j<dunew.size(); j++ ) dunew[j] = new GTVector<GFTYPE>(grid_->size());
-    for ( auto j=0; j<da   .size(); j++ ) da   [j] = new GTVector<GFTYPE>(grid_->size());
+    for ( auto j=0; j<utmp .size(); j++ ) utmp [j] = new StateComp(grid_->size());
+    for ( auto j=0; j<duold.size(); j++ ) duold[j] = new StateComp(grid_->size());
+    for ( auto j=0; j<dunew.size(); j++ ) dunew[j] = new StateComp(grid_->size());
+    for ( auto j=0; j<da   .size(); j++ ) da   [j] = new StateComp(grid_->size());
     u   .resize(grid_->size());
     diff.resize(grid_->size());
     du = NULLPTR;
@@ -155,7 +121,8 @@ int main(int argc, char **argv)
         u     [j] = pow(x,p)*pow(y,q)*pow(z,r);
       (*da[0])[j] = p==0 ? 0.0 : p*pow(x,p-1)*pow(y,q)*pow(z,r);
       (*da[1])[j] = q==0 ? 0.0 : q*pow(x,p)*pow(y,q-1)*pow(z,r);
-      if ( xnodes->size() > 2 ) (*da[2])[j] = r==0 ? 0.0 : r*pow(x,p)*pow(y,q)*pow(z,r-1);
+      if ( GDIM == 3 ) (*da[2])[j] = r==0 ? 0.0 
+                        : r*pow(x,p)*pow(y,q)*pow(z,r-1);
     } // end, loop over grid points
 
     for ( auto j=0; j<nc; j++ ) dnorm = MAX(dnorm, da[j]->amax());
@@ -178,10 +145,10 @@ int main(int argc, char **argv)
     } 
     GTimerStop("new_deriv");
 
-    GTVector<GFTYPE> 
-                      lnorm(2), gnorm(2), maxerrold(2), maxerrnew((2);
-    GTVector<GFTYPE> *maxerr;
-    std::string       smethod[2] = {"old", "new"};
+    GTVector<StateComp>
+                maxerr(2);
+    StateComp   lnorm(2), gnorm(2);
+    std::string smethod[2] = {"old", "new"};
 
 #if defined(_G_USE_GPTL)
     GPTLget_wallclock("old_deriv"     , 0,  &told); told /= ncyc;
@@ -191,33 +158,28 @@ int main(int argc, char **argv)
     /////////////////////////////////////////////////////////////////
     //////////////////////// Compute Errors /////////////////////////
     /////////////////////////////////////////////////////////////////
-    maxerrold = 0.0; maxerrnew = 0.0;
-    for ( auto n=0; n<1; n++ ) { // over old and new methods
-      for ( auto i=0; i<duoldw.size(); i++ ) du[i] = duold[i]; 
-      maxerr = &maxerrold;
-      if  ( n == 1 ) {
-        for ( auto i=0; i<dunew.size(); i++ ) du[i] = dunew[i];
-        maxerr = &maxerrnew;
-      }
-      for ( auto j=0; j<dunew.size(); j++ ) { // errors in each direction
+    for ( auto n=0; n<2; n++ ) { // over old and new methods
+      maxerr[n].resize(2); maxerr[n] = 0.0;
+      for ( auto i=0; i<du.size(); i++ ) du[i] = n==0? duold[i] : dunew[i];
+      for ( auto j=0; j<du.size(); j++ ) { // errors in each direction
         diff     = (*da[j]) - (*du[n])[j];
-       *utmp [0] = diff;                  // for inf-norm
-       *utmp [1] = diff; utmp[1]->pow(2); // for L2 norm
+       *utmp [0] = diff;                   // for inf-norm
+       *utmp [1] = diff; utmp[1]->rpow(2); // for L2 norm
 
         lnorm[0] = utmp[0]->infnorm (); 
-        gnorm[1] = sqrt(grid_->integrate(*utmp[2],*utmp[3]))/dnorm;
+        gnorm[1] = sqrt(grid_->integrate(*utmp[1],*utmp[2]))/dnorm;
 
         // Accumulate to find global inf-norm:
         GComm::Allreduce(lnorm.data()  , gnorm.data()  , 1, T2GCDatatype<GFTYPE>() , GC_OP_MAX, comm);
 cout << "main: gnorm[" << j << "]=" << gnorm << endl;
         // Now find max errors of each type for each field:
-        for ( auto i=0; i<maxerr->size(); i++ ) (*maxerr)[i] = MAX((*maxerr)[i],gnorm[i]);
-        if ( (*maxerr)[1] > eps ) {
+        for ( auto i=0; i<maxerr[n].size(); i++ ) maxerr[n][i] = MAX(maxerr[n][i],gnorm[i]);
+        if ( maxerr[n][1] > eps ) {
           std::cout << "main: -------------------------------------derivative FAILED : direction=" << j << " method: " << smethod[n]  << std::endl;
-          errcode = 1;
+          errcode += 1;
         } else {
           std::cout << "main: -------------------------------------derivative OK : direction=" << j << " method: " << smethod[n] << std::endl;
-          errcode = 0;
+          errcode += 0;
         }
 
       } // end, direction loop
@@ -237,11 +199,12 @@ cout << "main: gnorm[" << j << "]=" << gnorm << endl;
     }
     itst.close();
 
-    ios << pvec << 
-        << "  " << grid_->ngelems() 
-        <  "  " << ncyc
-        << "  " << maxerrold[0] << "  " << maxerrold[1] << "  " << told
-        << "  " << maxerrnew[0] << "  " << maxerrnew[1] << "  " << tnew
+    ios << pvec[0] ;
+        for ( auto j=1; j<GDIM; j++ ) ios << " " <<  pvec[j]; 
+    ios << "  " << grid_->ngelems() 
+        << "  " << ncyc
+        << "  " << maxerr[0][0] << "  " << maxerr[0][1] << "  " << told
+        << "  " << maxerr[1][0] << "  " << maxerr[1][1] << "  " << tnew
         << std::endl;
     ios.close();
  
--- test/sandbox/cdg/src/gtest_hack.h
+++ test/sandbox/cdg/src/gtest_hack.h
@@ -12,7 +12,9 @@
 #include <cstdio>
 #include <unistd.h>
 #include <iostream>
-#include "gptl.h"
+#if defined(_G_USE_GPTL)
+  #include "gptl.h"
+#endif
 #include <memory>
 #include <cstdlib>
 #include <cassert>
@@ -22,12 +24,59 @@
 #include "gllbasis.hpp"
 #include "gmass.hpp"
 #include "gmorton_keygen.hpp"
-#include "ggrid_factory.hpp"
 #include "gmtk.hpp"
+#include "ggrid_factory.hpp"
+#include "pdeint/observer_base.hpp"
+#include "pdeint/observer_factory.hpp"
 #include "tbox/property_tree.hpp"
 #include "tbox/mpixx.hpp"
 #include "tbox/global_manager.hpp"
 #include "tbox/input_manager.hpp"
 #include "gtools.h"
-#include "gio.h"
+
+
+#if 0
+typedef  GTVector<GTVector<GFTYPE>*> State;
+typedef  GTVector<GFTYPE>            StateComp;
+#endif
+
+using namespace geoflow::tbox;
+using namespace std;
+
+
+
+template< // Complete typepack
+typename StateType     = GTVector<GTVector<GFTYPE>*>,
+typename StateCompType = GTVector<GFTYPE>,
+typename StateInfoType = GStateInfo,
+typename GridType      = GGrid,
+typename MassOpType    = GMass,
+typename ValueType     = GFTYPE,
+typename DerivType     = StateType,
+typename TimeType      = ValueType,
+typename CompType      = GTVector<GStateCompType>,
+typename JacoType      = StateType,
+typename SizeType      = GSIZET
+>
+struct TypePack {
+        using State      = StateType;
+        using StateComp  = StateCompType;
+        using StateInfo  = StateInfoType;
+        using Grid       = GridType;
+        using Mass       = MassOpType;
+        using Value      = ValueType;
+        using Derivative = DerivType;
+        using Time       = TimeType;
+        using CompDesc   = CompType;
+        using Jacobian   = JacoType;
+        using Size       = SizeType;
+};
+using MyTypes       = TypePack<>;           // Define grid types used
+using Grid          = GGrid;
+using IOBaseType    = IOBase<MyTypes>;          // IO Base type
+using IOBasePtr     = std::shared_ptr<IOBaseType>;// IO Base ptr
+using ObsTraitsType = ObserverBase<MyTypes>::Traits;
+
+Grid      *grid_ = NULLPTR;
+IOBasePtr  pIO_  = NULLPTR; // ptr to IOBase operator
 
