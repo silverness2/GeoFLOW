447814a8ff8e9c5910698418cfd8a01f561a5cd1
Duane Rosenberg <Duane.Rosenberg@noaa.gov>
2020-11-10 21:50:07 -0700

    Make vector arg const in assign method

--- src/cdg/grid/gtpoint.hpp
+++ src/cdg/grid/gtpoint.hpp
@@ -95,7 +95,7 @@ public:
   inline GTPoint<T> &operator=(T f)
   { x1 = f;  x2 = f;  x3 = f;  x4 = f; return *this;}
 
-  inline void assign(GTVector<GTVector<T>> &v, GSIZET i)
+  inline void assign(const GTVector<GTVector<T>> &v, GSIZET i)
   { for ( auto j=0; j<gdim_; j++ ) *px_[j] = v[j][i];
     if ( gdim_>0) x1 = v[0][i]; if ( gdim_>1) x2 = v[1][i]; if (gdim_>2) x3 = v[2][i]; if ( gdim_>3) x4 = v[3][i];}
 
